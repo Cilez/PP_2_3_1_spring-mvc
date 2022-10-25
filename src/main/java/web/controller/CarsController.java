67a8562307c6b4df0544edd6a3c911dc9ab60396
Cilez <kobrelislegryi@gmail.com>
2022-10-25 15:46:03 +0300

    Create model package and class car.

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,12 @@ public class CarsController {
 
     @GetMapping(value = "/cars")
     public String printWelcomeCars(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm new controller");
-        messages.add("it's for cars!! ");
+        List<Car> messages = new ArrayList<>();
+        messages.add(new Car("Toyota","Seul",1234));
+        messages.add(new Car("Nisan","Seul",1234));
+        messages.add(new Car("Cheroki","Seul",1234));
+        messages.add(new Car("Compacy","Seul",1234));
+        messages.add(new Car("Foo","Seul",1234));
         model.addAttribute("messages", messages);
         return "index";
     }
